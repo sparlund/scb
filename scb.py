f8a6b8c34a81690adf55f62da1df4209c7fe567e
sparlund <johan.sparlund@gmail.com>
2018-09-22 15:55:19 +0200

    Test

--- scb.py
+++ scb.py
@@ -8,7 +8,7 @@ class scb():
 	Statistiska CentralByrån, SCB.
 	'''
 	# Base url for accessing database
-	url 	= 'http://api.scb.se/OV0104/v1/doris/sv/ssd/'
+	url 	= 'http://api.scb.se/OV0104/v1/doris/sv/ssd'
 	# Header for calling the db, what data you get back?
 	header 	= {'content-type': 'text/csv', 'accept': 'text/csv'}
 	
@@ -18,19 +18,23 @@ class scb():
 	def print(self):
 		r 		= requests.get(self.url+self.current_level)
 		data 	= json.loads(r.text)
-		
-
-		# If data is a dict, we are at a table, otherwise we're not
 		print('Current url is: '+self.url+self.current_level)
-		print(data)
-		if type(data) == type(list()):
-			print('Title: '+data['title'])
-			for variable in data['variables']:
-				print('	'+variable['code'])
+		if type(data) != type(dict()):
+			t = PrettyTable(['Table', 'Code'])
+			for table in data:
+				t.add_row([table['text'],table['id']])
+			print(t)	
 		else:
-			print('Categories for current level:')
-			for var in data['variables']:
-				print('	'+var['text'])
+			print('  Title: ')
+			print('	'+data['title'])
+			print('Filters:')
+			for filt in data['variables']:
+				print('	'+filt['text'])
+
+			print('You''ve reached a table, please use method get() to filter data.')
+						
+
+
 
 	def enter(self,id):
 		# Get current available categories
@@ -67,20 +71,50 @@ class scb():
 		print('Requesting data from table: '+data['title'])
 
 		post_query = dict()
-		# What type of data to return!
 		post_query['query'] = []
-		for variable in data['variables']:
-			t = PrettyTable([variable['text'], 'Code'])
-			for row in range(0,len(variable['values'])):
-				t.add_row([variable['valueTexts'][row],variable['values'][row]])
-			print(t)
-			filter_chosen = input('Enter filters from above: ')
-			if filter_chosen != '-':
-				# Make filter_chosen separata items i en lista, inte som saker på rad!!!
-				post_query['query'].append(
-					{'code': variable['code'], 'selection': {'filter': 'item', 'values': filter_chosen.split(',')}})
-				post_query['response'] = {'format': 'json'}
+		'''
+		We want to print the content code filter first, makes more sense to select
+		that first. So we want to find the index of the variable that has the code
+		ContentsCode, and what index it has in the recieved json data.
+		'''
+		contentcode_index = int([counter for counter,entry in enumerate(data['variables']) if entry['code'] == 'ContentsCode'][0])
+		for j,variable in enumerate(data['variables']):
+			while True:
+				# Print ContentCode variable filter first!
+				if j == 0:
+					contentcode = data['variables'][contentcode_index]
+					t = PrettyTable([contentcode['text'], 'Code'])
+					available_filters = contentcode['values']					
+					for row in range(0,len(contentcode['values'])):
+						t.add_row([contentcode['valueTexts'][row],contentcode['values'][row]])
+				else:		
+					t = PrettyTable([variable['text'], 'Code'])					
+					available_filters = variable['values']
+					for row in range(0,len(variable['values'])):
+						t.add_row([variable['valueTexts'][row],variable['values'][row]])
+				print(t)
+				filter_chosen = input('Enter filters from above: ')
+				# Check if the enterd value is in the available values
+				# If it's not, print the table again and let the user enter again.
+				if filter_chosen not in available_filters and filter_chosen != '':
+					print('You entered code:'+str(filter_chosen)+', and it''s not available.')
+				else:
+					# User entered correct value(s), break while loop and go to next filter	
+					if filter_chosen != '':
+						# Make filter_chosen separata items i en lista, inte som saker på rad!!!
+						if j == 0:
+							code = data['variables'][contentcode_index]['code'] 
+							del data['variables'][contentcode_index]
+							print(data['variables'])
+						else:
+							code = variable['code'] 	
+						post_query['query'].append(
+							{'code': code, 'selection': {'filter': 'item', 'values': filter_chosen.split(',')}})
+						post_query['response'] = {'format': 'json'}
+					break	
+
 		# Finally, make a post request to the database at the current level with the specified filters
+		print(post_query)
 		r = requests.post(self.url + self.current_level,data=json.dumps(post_query),headers=self.header)
 		jd = json.loads(r.content)
 		# Create output dict
@@ -118,8 +152,12 @@ class scb():
 
 
 
-#s = scb()
-#s.enter('BE')
+s = scb()
+s.enter('OE')
+s.enter('OE0108')
+s.enter('OffEkoMott')
+s.print()
+o=s.get()
 #s.enter('BE0101')
 #s.enter('BE0101A')
 #s.enter('BefolkningNy')
